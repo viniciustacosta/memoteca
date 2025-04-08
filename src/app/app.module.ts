382254bb758fbd01976b6f181afbfbb22d6bb6fa
Vinícius Tadeu Andrade Costa <160060199+viniciustacosta@users.noreply.github.com>
2025-04-08 12:08:54 -0300

    Finalizaçao do Módulo 3 - Curso 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pens
 import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+import { BotaoCarregarMaisComponent } from './componentes/pensamentos/listar-pensamento/botao-carregar-mais/botao-carregar-mais.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pens
     PensamentoComponent,
     ExcluirPensamentoComponent,
     EditarPensamentoComponent,
+    BotaoCarregarMaisComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/pensamentos/listar-pensamento/botao-carregar-mais/botao-carregar-mais.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-carregar-mais',
+  standalone: false,
+  templateUrl: './botao-carregar-mais.component.html',
+  styleUrl: './botao-carregar-mais.component.css'
+})
+export class BotaoCarregarMaisComponent implements OnInit {
+  @Input() haMaisPensamentos: boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+}
